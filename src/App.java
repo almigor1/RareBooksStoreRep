5c8c2fd07166612c4011f58e877a012c46500fec
Alexey Gorokhov <almigor@mail.ru>
2024-11-15 16:36:59 +0300

    Revert "черри пик"
    This reverts commit a32c4fb7938582f2fe9148eb87933b3dcafbe7bd.

--- src/App.java
+++ src/App.java
@@ -1,8 +1,6 @@
 public class App {
     public static void main (String [] args)
     {
-        System.out.println("Мы открылись!!!");
-
-
+        System.out.println("Открытие через полчаса!");
     }
 }
