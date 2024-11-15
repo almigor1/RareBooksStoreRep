120f35db4fd7abef9ac580cbf71caa3f529a0552
Alexey Gorokhov <almigor@mail.ru>
2024-11-15 15:59:21 +0300

    5 days

--- src/App.java
+++ src/App.java
@@ -1,8 +1,7 @@
 public class App {
     public static void main (String [] args)
     {
-        System.out.println("Было: До открытия магазина 6 месяцев!!!");
-        System.out.println("Стало: До открытия магазина 5 дней!");
+        System.out.println("До открытия магазина 1 день!");
 
     }
 }
