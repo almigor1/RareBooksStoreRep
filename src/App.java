c2c2612be8452c8b07580a07ab94db6541df41bd
Alexey Gorokhov <almigor@mail.ru>
2024-11-15 16:00:23 +0300

    через полчаса

--- src/App.java
+++ src/App.java
@@ -1,8 +1,6 @@
 public class App {
     public static void main (String [] args)
     {
-        System.out.println("Было: До открытия магазина 6 месяцев!!!");
-        System.out.println("Стало: До открытия магазина 5 дней!");
-
+        System.out.println("Открытие через полчаса!");
     }
 }
